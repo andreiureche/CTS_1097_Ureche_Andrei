cf6414f3cc7491ac2f21785d1bdeedd82b6fe8a1
andreiureche <andreiureche04@gmail.com>
2026-05-08 11:46:48 +0300

    Added eleventh seminary

---
+++ CTS_Seminar11/src/chainofresponsability/Notificator.java
@@ -0,0 +1,15 @@
+package chainofresponsability;
+
+public abstract class Notificator {
+    private Notificator succesor;
+
+    public void setSuccesor(Notificator succesor){
+        this.succesor=succesor;
+    }
+
+    public Notificator getSuccesor() {
+        return succesor;
+    }
+
+    public abstract void trimiteNotificare(Client client, String mesaj);
+}
